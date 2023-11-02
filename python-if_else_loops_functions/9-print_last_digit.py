7fc0d7a485fa87e0d2ce2758c2830ad697e1e5ba
Cchancee <144018517+Cchancee@users.noreply.github.com>
2023-11-02 09:13:19 +0200

    changes

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# This function prints the last digit of a number
+# @number: The number to be inputed by the user
+
+
+def print_last_digit(number):
+    if number > 0:
+        print(f"{number % 10}", end="")
+        return (number % 10)
+    elif number < 0:
+        print(f"{(-number % 10)}", end="")
+        return ((-number % 10))
+    else:
+        print(f"{number:}", end="")
+        return (number)i
