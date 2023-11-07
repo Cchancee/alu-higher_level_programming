3b57c76bd5796126c168230e668db42275056246
Cchancee <144018517+Cchancee@users.noreply.github.com>
2023-11-07 09:10:09 +0200

    modify

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
+# Replaces all occurrences of an element by another in a new list
 
 
 def search_replace(my_list, search, replace):
-    for i in range (len(my_list)):
-        if my_list[i] == search:
-            my_list[i] = replace
-    new_list = my_list
-    return new_list
+    newlist = []
+    newlist = [replace if i == search else i for i in my_list]
+    return newlist
 
 
-
-
+'''
 my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-
-print(search_replace(my_list, 2,89))
+print(search_replace(my_list, 2, 89))
+'''
