f2b2773a1beb21008b726668dd586790b30910a1
Cchancee <144018517+Cchancee@users.noreply.github.com>
2023-11-07 08:34:46 +0200

    modify

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -13,3 +13,13 @@ def square_matrix_simple(matrix=[]):
         d = [i*i for i in matrix[row]]
         k.append(d)
     return k
+
+'''
+matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
+
+print(square_matrix_simple(matrix))
+'''
