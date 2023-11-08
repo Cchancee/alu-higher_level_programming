ac79ce6bf44a3d5035f8f6053ddbf732899ab2c1
Cchancee <144018517+Cchancee@users.noreply.github.com>
2023-11-08 09:49:52 +0200

    new

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+        except IndexError:
+            break
+    print()
+    return count
+
+'''
+my_list = [1, 2, 3, 4, 5]
+print(safe_print_list(my_list, 2))
+'''
