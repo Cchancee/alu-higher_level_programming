73e0d50bde0896ef9b362449b4691dcd22500482
Cchancee <144018517+Cchancee@users.noreply.github.com>
2023-11-09 09:32:12 +0200

    new

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
                 print("{:d}".format(my_list[i]), end='')
                 nb_print += 1
-        except (IndexError, TypeError, ValueError):
+        except (TypeError, ValueError):
               continue
-              nb_print += 1
     print()
     return nb_print
