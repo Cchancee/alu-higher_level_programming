88b2d0d737bec56611fd73c21f6f018d36acd4e6
Cchancee <144018517+Cchancee@users.noreply.github.com>
2023-11-10 07:29:55 +0200

    new

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    for i in range(list_length):
+        try: 
+            qt = my_list_1[i] / my_list_2[i]
+        except TypeError:
+            print("Wront type")
+          res = 0
+        except ZeroDivisionError:
+            print("Division by 0")
+            res = 0
+        except IndexError:
+            print("Out of range")
+            res = 0
+        finally:
+            new_list.append(res)
+    return new_list
