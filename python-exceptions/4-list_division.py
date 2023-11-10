a5cba97777e15cd5a7526111d7f13606c3ca3213
Cchancee <144018517+Cchancee@users.noreply.github.com>
2023-11-10 07:30:56 +0200

    new

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -7,7 +7,7 @@ def list_division(my_list_1, my_list_2, list_length):
             qt = my_list_1[i] / my_list_2[i]
         except TypeError:
             print("Wront type")
-          res = 0
+            res = 0
         except ZeroDivisionError:
             print("Division by 0")
             res = 0
