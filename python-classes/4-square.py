fc3c00d6435a41cd783b1c4a478d28c2f0c1d20f
Cchancee <144018517+Cchancee@users.noreply.github.com>
2023-11-13 13:13:50 +0200

    classes

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,16 +5,18 @@
 class Square():
     '''Definition of class here'''
     def __init__(self, size):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
         if not isinstance(size, int):
               raise TypeError("size must be an integer")
         elif size < 0:
               raise ValueError("size must be >= 0")
-        self.__size = size
-
-    def size(self):
-        return self.__size
-
-    def size(self, value):
         self.__size = value
 
     def area(self):
