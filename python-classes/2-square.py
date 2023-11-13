8ce979a4874cfd75be92eb8f3aa3ddff8dc9b980
Cchancee <144018517+Cchancee@users.noreply.github.com>
2023-11-13 12:57:13 +0200

    classes

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-'''Define class'''
+"""Define a Square class."""
 
-class Square():
-    '''This is class definition'''
+
+class Square:
+    """Define a square."""
+
     def __init__(self, size=0):
-            if not isinstance(size, int):
-                    raise TypeError("size must be an integer")
-            elif size < 0:
-                    raise ValueError("size must be >= 0")
-             self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
