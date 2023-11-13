3a7a1c0bbe7c452796a83afabb58a94a2353a050
Cchancee <144018517+Cchancee@users.noreply.github.com>
2023-11-13 12:50:36 +0200

    classes

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,11 +5,11 @@
 class Square():
         '''This is class definition'''
         def __init__(self, size=0):
+                
                 '''Defining constructor'''
-
-              '''Validating the private instance attribute'''
-              if not isinstance(size, int):
-                 raise TypeError("size must be an integer")
-              elif size < 0:
-                  raise ValueError("size must be >= 0")
-              self.__size = size
+                '''Validating the private instance attribute'''
+                if not isinstance(size, int):
+                         raise TypeError("size must be an integer")
+                elif size < 0:
+                         raise ValueError("size must be >= 0")
+                self.__size = size
