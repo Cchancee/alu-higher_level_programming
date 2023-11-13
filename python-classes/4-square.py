dc5ffd53259f32157528916115d50cf95b700270
Cchancee <144018517+Cchancee@users.noreply.github.com>
2023-11-13 13:06:50 +0200

    classes

---
+++ python-classes/4-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Defining the class Square'''
+
+
+class Square():
+    def __init__(self, size):
+        if not isinstance(size, int):
+              raise TypeError("size must be an integer")
+        elif size < 0:
+              raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        self.__size = value
