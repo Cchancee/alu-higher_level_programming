39031e8e7f904fe5ffc79774c1c04ffc55de1c90
Cchancee <144018517+Cchancee@users.noreply.github.com>
2024-02-04 21:19:12 +0200

    Update 101-sorted.js

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
+
 const dict = require('./101-data.js').dict;
-let newDict = {};
-for (let key in dict) {
-  if (newDict[dict[key]] === undefined) {
-    newDict[dict[key]] = [key];
+const newDict = {};
+for (const key in dict) {
+  const value = dict[key];
+  if (newDict[value] === undefined) {
+    newDict[value] = [];
+    newDict[value].push(key);
   } else {
-    newDict[dict[key]].push(key);
+    newDict[value].push(key);
   }
 }
 console.log(newDict);
